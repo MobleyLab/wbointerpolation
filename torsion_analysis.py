95f5bcda0a9622d832af52ab7890ac22100afdf0
Pavan Behara <pavan.behara@openforcefield.org>
2020-12-01 23:56:40 -0800

    visualization function show_oemol_struc() added

--- torsion_analysis.py
+++ torsion_analysis.py
@@ -573,3 +573,46 @@ def plot_tid_for_datasets(fileList, t_id):
     )
     fig.show()
     return fig
+
+def show_oemol_struc(oemol, torsions=False, atom_indices=[]):
+    from IPython.display import Image
+    from openeye import oedepict, oechem
+    width = 500
+    height = 300
+    
+    
+    # Highlight element of interest
+    class NoAtom(oechem.OEUnaryAtomPred):
+        def __call__(self, atom):
+            return False
+    class AtomInTorsion(oechem.OEUnaryAtomPred):
+        def __call__(self, atom):
+            return atom.GetIdx() in atom_indices
+    class NoBond(oechem.OEUnaryBondPred):
+        def __call__(self, bond):
+            return False
+    class BondInTorsion(oechem.OEUnaryBondPred):
+        def __call__(self, bond):
+            return (bond.GetBgn().GetIdx() in atom_indices) and (bond.GetEnd().GetIdx() in atom_indices)
+    class CentralBondInTorsion(oechem.OEUnaryBondPred):
+        def __call__(self, bond):
+            return (bond.GetBgn().GetIdx() in atom_indices[1:3]) and (bond.GetEnd().GetIdx() in atom_indices[1:3])
+
+        
+    opts = oedepict.OE2DMolDisplayOptions(
+                        width, height, oedepict.OEScale_AutoScale
+                    )
+    opts.SetAtomPropertyFunctor(oedepict.OEDisplayAtomIdx())
+    
+    oedepict.OEPrepareDepiction(oemol)
+    img = oedepict.OEImage(width, height)
+    display = oedepict.OE2DMolDisplay(oemol, opts)
+    if(torsions):
+        atoms = oemol.GetAtoms(AtomInTorsion())
+        bonds = oemol.GetBonds(NoBond())
+        abset = oechem.OEAtomBondSet(atoms, bonds)
+        oedepict.OEAddHighlighting(display, oechem.OEColor(oechem.OEYellow), oedepict.OEHighlightStyle_BallAndStick, abset)
+    
+    oedepict.OERenderMolecule(img, display)
+    png = oedepict.OEWriteImageToString("png", img)
+    return Image(png)
