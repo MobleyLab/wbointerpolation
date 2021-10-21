da107a33edf19cc2ad45c2d3d2c9a0b4a8c4a733
danieljcabrera <djcabrera@roadrunner.com>
2021-10-21 15:44:57 -0700

    Updated failed molecules from filter with error messages

--- WBO_dataset_analysis/doublering_filter.py
+++ WBO_dataset_analysis/doublering_filter.py
@@ -10,6 +10,7 @@ import argparse
 from openeye import oechem, oeomega
 from openff.toolkit.topology import Molecule
 import pickle
+import sys
 
 def filter():
     parser = argparse.ArgumentParser()
@@ -27,8 +28,8 @@ def filter():
     total_fail = 0
     group_num = 0
     failed_group_num = 0
-    molecules_match={}
-    fails_openff_mol=[]
+    molecules_match = {}
+    fails_openff_mol = []
     smarts = "[#6X3H1:1]~[#6X3:2](~[#6X3H1])-[#6X3:3](~[#6X3H1])~[#6X3H1:4]"
     
     with open(args.smiles_database, "r") as file:
@@ -41,14 +42,14 @@ def filter():
                 molecule = Molecule.from_smiles(smiles, allow_undefined_stereo=True)
             except KeyboardInterrupt:
                 break
-            except:
-                fails_openff_mol.append(smiles)
+            except Exception as e:
+                fails_openff_mol.append( (smiles, str(e)) )
                 fail_count += 1
                 if fail_count == 10000:
                     with open(f"openff_fails/failed_mols_group{failed_group_num}.pkl", 'wb') as handle:
                         pickle.dump(fails_openff_mol, handle, protocol=pickle.HIGHEST_PROTOCOL)
                     failed_group_num += 1
-                    fails_openff_mol=[]
+                    fails_openff_mol = {}
                     total_fail += fail_count
                     fail_count = 0
                 continue
@@ -66,23 +67,25 @@ def filter():
                     match_count = 0
             except KeyboardInterrupt:
                 break
-            except:
-                fails_openff_mol.append(smiles)
+            except Exception as e:
+                fails_openff_mol.append( (smiles, str(e)) )
                 fail_count += 1
                 if fail_count == 10000:
                     with open(f"openff_fails/failed_mols_group{failed_group_num}.pkl", 'wb') as handle:
                         pickle.dump(fails_openff_mol, handle, protocol=pickle.HIGHEST_PROTOCOL)
                     failed_group_num += 1
-                    fails_openff_mol=[]
+                    fails_openff_mol = {}
                     total_fail += fail_count
                     fail_count = 0
                 continue
+            
                 
     with open(f"openff_results/mols_group{group_num}.pkl", "wb") as handle:
         pickle.dump(molecules_match, handle, protocol=pickle.HIGHEST_PROTOCOL)
-        
+     
+    
     with open(f"openff_fails/failed_mols_group{failed_group_num}.pkl", "wb") as handle:
-        pickle.dump(fails_openff_mol, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        pickle.dump(fails_openff_mol, handle)#, protocol=pickle.HIGHEST_PROTOCOL)
     
     total_match += match_count
     total_fail += fail_count
